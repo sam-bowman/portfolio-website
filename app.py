3e66256e2c23e238c51377902b72baf67f75103e
Hoshiwara <onishiwatsu@gmail.com>
2024-05-20 19:25:29 +0100

    switch to pyvibe

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, url_for
+import pyvibe as pv
+from pf_components import global_components
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    page = pv.Page(
+        title='Home - SB',
+        description="Homepage",
+        navbar=global_components.get_navbar("Homepage"),
+        footer=None,
+        sidebar=global_components.get_sidebar()
+    )
+    page.add_header("You're here too early! This page will be updated soon(tm)")
+
+    return page.to_html()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
